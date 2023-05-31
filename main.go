2b5bf52ba53473046b4da34fa4e724ec7c4ab259
Essam Said <100171793+EssamSaid24@users.noreply.github.com>
2023-05-31 23:16:54 +0300

    variable declaration

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main(){
-	fmt.Println("Hello Essam!")
+func main() {
+	// declare string 
+	var firstName string = "Essam "
+	var secondName = "Said "
+	lastName := "Shalaby"
+	fmt.Println(firstName,secondName,lastName)
+
+	// declare int 
+
+	var age int = 22
+	var birthYear = 2000
+	month := 11
+	fmt.Println(age, birthYear, month)
+
+	// bits and memory 
+
+	var signedNumOne int8 = -25
+	var unSignedNumTwo uint16 = 255
+	fmt.Println(signedNumOne,unSignedNumTwo)
+
 }
